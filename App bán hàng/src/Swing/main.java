c6ce8c4ad259b8b7378d90fe504f392ec463cc3d
Nguyễn Ngọc Báu <65180347+ngocbau12324@users.noreply.github.com>
2022-11-04 17:14:28 +0700

    Delete App bán hàng directory

--- App bán hàng/src/Swing/main.java
+++
@@ -1,9 +0,0 @@
-package Swing;
-public class main extends Register_Form {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Register_Form f = new Register_Form();
-	}
-
-}
